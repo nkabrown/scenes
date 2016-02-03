ae5c0af691eee2786029751f22c64d7f2e7fd61f
Nathan Brown <nkabrown@gmail.com>
2016-02-02 20:03:30 -0500

    Add third page view

--- assets/js/scene-controller.js
+++ assets/js/scene-controller.js
@@ -1,5 +1,12 @@
+// trigger first page transition
 $('div.fill-01').on('click', function() {
   $('div.fill-01').addClass('animate-left');
-  d3.select('div.presentation').transition().delay(900).style('background-color', '#78cfbc');
-  window.setTimeout(function() { $('div.introduction').hide(); }, 1200);
+  d3.select('div.view-two').transition().delay(900).style('background-color', '#78cfbc');
+  window.setTimeout(function() { $('div.view-one').hide(); }, 1200);
+});
+
+// trigger second page transition
+$('div.view-two').on('click', function() {
+  $('div.view-three').addClass('animate-right');
+  $('div.view-three').css('left', '0');
 });
