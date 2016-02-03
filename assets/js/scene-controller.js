b487199f57324775ecd13c65793516f816ae2c4f
Nathan Brown <nkabrown@gmail.com>
2016-02-03 15:46:06 -0500

    Add navigation functionality

--- assets/js/scene-controller.js
+++ assets/js/scene-controller.js
@@ -1,28 +1,66 @@
 // trigger first page transition
-$('div.fill-01').on('click', function() {
-  $('div.fill-01').addClass('animate-left');
-  d3.select('div.view-two').transition().delay(900).style('background-color', '#78cfbc');
-  window.setTimeout(function() { $('div.view-one').hide(); }, 1200);
+$('div.view-one').on('click', function() {
+  $('div.view-two').show().addClass('animate-left').css('left', '0');
+  $('.dot-one').show();
+  window.setTimeout(function() { $('div.view-one').hide(); }, 2000);
 });
 
 // trigger second page transition
 $('div.view-two').on('click', function() {
-  $('div.view-three').addClass('animate-right').css('left', '0');
+  $('div.view-three').show().addClass('animate-right').css('left', '0');
+  $('.dot-two').show();
+  window.setTimeout(function() { $('div.view-two').removeClass('animate-left').hide(); }, 2000);
 });
 
 // trigger third page transition
 $('div.view-three').on('click', function() {
-  $('div.view-two').hide();
-  $('div.wing-one').addClass('enter-left').css('left', '0');
-  $('div.wing-two').addClass('enter-right').css('right', '0');
+  $('div.wing-one').show().addClass('enter-left').css('left', '0');
+  $('div.wing-two').show().addClass('enter-right').css('right', '0');
+  $('.dot-three').show();
 });
 
 // trigger fourth page transition
 $('div.wing-one, div.wing-two').on('click', function() {
   $('div.view-five').show().addClass('animate-up').css('top', '0');
+  window.setTimeout(function() { $('div.view-three, div.wing-one, div.wing-two').removeClass('enter-left', 'enter-right', 'animate-right').hide(); }, 2000);
+  $('.dot-four').show();
 });
 
 // trigger fifth page transition
 $('div.view-five').on('click', function() {
-  $('div.view-six').addClass('animate-down').css('top', '0');
+  $('div.view-six').show().addClass('animate-down').css('top', '0');
+  window.setTimeout(function() { $('div.view-five').removeClass('animate-up').hide(); }, 2000);
+  $('.dot-five').show();
+  $('.dot-six').show();
+});
+
+// controls for vertical nav bar
+$('a[href$="view-one"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-one').show();
+});
+
+$('a[href$="view-two"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-two').show();
+});
+
+$('a[href$="view-three"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-three').show();
+});
+
+$('a[href$="view-four"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-three, div.wing-one, div.wing-two').show();
+});
+
+$('a[href$="view-five"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-five').show();
+});
+
+$('a[href$="view-six"]').on('click', function() {
+  $('.master > div:visible').hide();
+  $('div.view-six').show();
 });
